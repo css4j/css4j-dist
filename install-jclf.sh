c7581b3a2ddc36b9a494ff1ccbe3747bbc381a21
Carlos Amengual <carlosame644+github@gmail.com>
2020-11-12 22:16:11 +0100

    Install scripts: remove the _remote.repositories file.

--- install-jclf.sh
+++ install-jclf.sh
@@ -9,6 +9,7 @@ ARTIFACT=jclf
 VERSION=4.1.0
 REMOTEREPO=https://css4j.github.io/maven/
 PLUGIN=org.apache.maven.plugins:maven-dependency-plugin:2.9:get
+LOCALREPO=${HOME}/.m2/repository
 #
 mvn $PLUGIN -DremoteRepositories=${REMOTEREPO} -Dartifact=${GROUP}:${ARTIFACT}:${VERSION} -Ddest=${TMP}/${ARTIFACT}-${VERSION}.jar
 mvn $PLUGIN -DremoteRepositories=${REMOTEREPO} -Dartifact=${GROUP}:${ARTIFACT}:${VERSION} -Dpackaging=pom -Ddest=${TMP}/pom.xml
@@ -16,3 +17,4 @@ mvn $PLUGIN -DremoteRepositories=${REMOTEREPO} -Dartifact=${GROUP}:${ARTIFACT}:$
 mvn install:install-file -Dfile=${TMP}/${ARTIFACT}-${VERSION}.jar -DpomFile=${TMP}/pom.xml -DgroupId=${GROUP} -DartifactId=${ARTIFACT} -Dversion=${VERSION} -Dpackaging=jar
 mvn install:install-file -Dfile=${TMP}/${ARTIFACT}-${VERSION}-sources.jar -DgroupId=${GROUP} -DartifactId=${ARTIFACT} -Dversion=${VERSION} -Dpackaging=jar -Dclassifier=sources
 rm ${TMP}/${ARTIFACT}-${VERSION}.jar ${TMP}/${ARTIFACT}-${VERSION}-sources.jar ${TMP}/pom.xml
+rm ${LOCALREPO}/io/sf/jclf/jclf/*/_remote.repositories
