fc3e3fae9ba5eecfdba2dffbb9e6ba9de4a71914
Carlos Amengual <carlosame644+github@gmail.com>
2021-07-26 14:27:01 +0200

    Maven jclf script: current code uses jclf-text 5.0.0.

--- install-jclf.sh
+++ install-jclf.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 #
-# Install the JCLF artifact if it is not present.
+# Install the JCLF-text artifact if it is not present.
 #
 # Assumes that the $TMP environment variable points to a temporary directory.
 #
 GROUP=io.sf.jclf
-ARTIFACT=jclf
-VERSION=4.1.0
+ARTIFACT=jclf-text
+VERSION=5.0.0
 REMOTEREPO=https://css4j.github.io/maven/
 PLUGIN=org.apache.maven.plugins:maven-dependency-plugin:2.9:get
 LOCALREPO=${HOME}/.m2/repository
@@ -17,4 +17,4 @@ mvn $PLUGIN -DremoteRepositories=${REMOTEREPO} -Dartifact=${GROUP}:${ARTIFACT}:$
 mvn install:install-file -Dfile=${TMP}/${ARTIFACT}-${VERSION}.jar -DpomFile=${TMP}/pom.xml -DgroupId=${GROUP} -DartifactId=${ARTIFACT} -Dversion=${VERSION} -Dpackaging=jar
 mvn install:install-file -Dfile=${TMP}/${ARTIFACT}-${VERSION}-sources.jar -DgroupId=${GROUP} -DartifactId=${ARTIFACT} -Dversion=${VERSION} -Dpackaging=jar -Dclassifier=sources
 rm ${TMP}/${ARTIFACT}-${VERSION}.jar ${TMP}/${ARTIFACT}-${VERSION}-sources.jar ${TMP}/pom.xml
-rm ${LOCALREPO}/io/sf/jclf/jclf/*/_remote.repositories
+rm ${LOCALREPO}/io/sf/jclf/${ARTIFACT}/*/_remote.repositories
