8b38a168fab8e22441bf939e4c2ee9bc20ce6b83
Carlos Amengual <carlosame644+github@gmail.com>
2022-03-02 14:40:01 +0100

    Maven scripts: install css4j-dom4j 3.7.1

--- maven/install-css4j.sh
+++ maven/install-css4j.sh
@@ -49,7 +49,7 @@ VERSION=3.6.0
 install "${GROUP}" "${ARTIFACT}" "${VERSION}"
 #
 ARTIFACT=css4j-dom4j
-VERSION=3.7.0
+VERSION=3.7.1
 install "${GROUP}" "${ARTIFACT}" "${VERSION}"
 #
 rm -f ${LOCALREPO}/io/sf/carte/*/*/_remote.repositories
